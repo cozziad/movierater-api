ff8dd5a55ea90650aaeb9c2de6e11a275a208921
Anthony <cozziad@gmail.com>
2020-07-04 11:10:23 -0400

    API building

--- movierater/api/urls.py
+++ movierater/api/urls.py
@@ -1,8 +1,12 @@
-from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
+from rest_framework import routers
+from .views import MovieViewSet, RatingViewSet
+
+router = routers.DefaultRouter()
+router.register('movies', MovieViewSet)
+router.register('rating', RatingViewSet)
 
 urlpatterns = [
-    path('boo/', admin.site.urls),
-
+    path('', include(router.urls)),
 ]
